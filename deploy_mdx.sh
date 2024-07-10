ab8c12dc42160b2c45fb8c07fc7f0a2f1594f5dd
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-07-10 17:35:04 +0900

    feat: add data

--- deploy_mdx.sh
+++ deploy_mdx.sh
@@ -1,1 +1,18 @@
-s3cmd sync docs/rekion/ s3://rekion/iiif/ --exclude '.DS_Store' && s3cmd setacl s3://rekion/iiif/ --acl-public --recursive
+#!/bin/bash
+
+# s3cmd sync docs/rekion/ s3://rekion/iiif/ --exclude '.DS_Store' && s3cmd setacl s3://rekion/iiif/ --acl-public --recursive
+
+# ログファイルのパス
+LOGFILE="sync_output.log"
+
+# s3cmd sync コマンドの実行とログの保存
+s3cmd sync docs/rekion/ s3://rekion/iiif/ --exclude '.DS_Store' > $LOGFILE
+
+grep "upload:" $LOGFILE | awk -F"'" '{print $4}' | while read dst
+do
+    s3cmd setacl "${dst}" --acl-public
+done
+
+# ログファイルの削除
+rm $LOGFILE
+
